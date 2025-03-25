98b1e9c4bda4afdadd600f5319a2163bca0df93f
gizzi <gianluca.gizzi@cefriel.com>
2025-03-25 12:55:33 +0100

    Aggiunti file xml per configurazione camel route

--- src/main/java/de/unioninvestment/CamundaCamelBootApplication.java
+++ src/main/java/de/unioninvestment/CamundaCamelBootApplication.java
@@ -1,14 +1,64 @@
 package de.unioninvestment;
 
-import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import java.io.File;
+import java.util.Arrays;
+
+//import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.apache.camel.spring.Main;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
-@EnableProcessApplication
+
+///**
+// * @SpringBootApplication is a convenience annotation that adds all of the following:
+// *  - @Configuration: Tags the class as a source of bean definitions for the application context.
+// *  - @EnableAutoConfiguration: Tells Spring Boot to start adding beans based on classpath settings, other beans, and various property settings.
+// *  - @ComponentScan: Tells Spring to look for other components, configurations, and services in the src/main package, letting it find the controllers
+// */ 
+//@SpringBootApplication
+//public class CamundaCamelBootApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication application = new SpringApplication(CamundaCamelBootApplication.class);
+//		application.setBannerMode(Banner.Mode.CONSOLE);
+//		application.setLazyInitialization(false);
+//		application.run(args);
+//	}
+//	
+////	@Bean
+////	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+////		return args -> {
+////
+////			System.out.println("Let's inspect the beans provided by Spring Boot:");
+////
+////			String[] beanNames = ctx.getBeanDefinitionNames();
+////			Arrays.sort(beanNames);
+////			for (String beanName : beanNames) {
+////				System.out.println(beanName);
+////			}
+////
+////		};
+////	}
+//}
+
+
 public class CamundaCamelBootApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CamundaCamelBootApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		Main main = new Main();
+		String path = "camel-context.xml";
+		File f = new File("./" + path);
+		if(f.exists() && !f.isDirectory()) {
+            System.out.println("Routes in current directory");
+			main.setFileApplicationContextUri("./" + path);
+		} else {
+            System.out.println("Routes in dedicated directory");
+			main.setApplicationContextUri("classpath:/routes/send-to-camel-config.xml");
+		}
+		main.run();
 	}
 }
+
--- src/main/java/de/unioninvestment/camel/CamelCamundaRoute.java
+++ src/main/java/de/unioninvestment/camel/CamelCamundaRoute.java
@@ -1,30 +1,73 @@
-package de.unioninvestment.camel;
-
-import org.apache.camel.builder.RouteBuilder;
-import org.springframework.stereotype.Component;
-
-@Component
-public class CamelCamundaRoute extends RouteBuilder{
-	
-	@Override
-	public void configure() throws Exception {
-		
-
-		from("timer://camundaStarter?fixedRate=true&period=60000")
-		.id("camel-camunda-starter")
-		.to("log:start_camunda_bpmn_from_camel_route")
-		.setBody(constant("test"))
-		.to("camunda-bpm://start?processDefinitionKey=camunda-camel");
-
-		
-		
-		from("direct:2camel")
-		.routeId("2camel") //
-		.id("2camel")
-		.to("log:camunda_bpmn_called_camel_route");
-
-
-	
-
-	}
-}
+//package de.unioninvestment.camel;
+//
+//import java.util.Set;
+//
+//import org.apache.camel.CamelContext;
+//import org.apache.camel.builder.RouteBuilder;
+//import org.springframework.boot.availability.AvailabilityChangeEvent;
+//import org.springframework.boot.availability.ReadinessState;
+//import org.springframework.context.event.EventListener;
+//import org.springframework.stereotype.Component;
+//import org.apache.camel.model.rest.RestBindingMode;
+//
+//@Component
+//public class CamelCamundaRoute extends RouteBuilder{
+//	
+//	
+//	@EventListener
+//	public void onStateChange(AvailabilityChangeEvent<ReadinessState> event) {
+//		switch (event.getState()) {
+//			case ACCEPTING_TRAFFIC -> {
+//				System.out.println("Camel Route builder is ready to accepts messages");
+//			}
+//			case REFUSING_TRAFFIC -> {
+//				System.out.println("Camel Route builder is not ready to accepts messages");
+//			}
+//		}
+//	}
+//	
+//	@Override
+//	public void configure() throws Exception {
+//		
+//		restConfiguration()
+//			.component("undertow")
+//			.bindingMode(RestBindingMode.off)	        
+//			.contextPath("/")
+//			.host("0.0.0.0")
+//			.port(8085);
+//		
+//		rest("/")
+//    		.get("test")	    	
+//    		.to("direct:manageTestRest");
+//		
+//	    from("direct:manageTestRest")
+//    		.routeId("manage-test-rest-requests")
+//    		.log("Test rest recieved correctly");
+//
+//	    
+////		from("timer://camundaStarter?fixedRate=true&period=60000")
+////			.id("camel-camunda-starter")
+////			.to("log:start_camunda_bpmn_from_camel_route")
+////			.setBody(constant("test"))
+////			.to("camunda-bpm://start?processDefinitionKey=camunda-camel");
+//		
+//		from("direct:2camel")
+//			.routeId("2camel") 
+//			.id("2camel")
+//			.to("log:camunda_bpmn_called_camel_route");
+//
+//		
+//	}
+//
+//	@Override
+//	public void addTemplatedRoutesToCamelContext(CamelContext context) throws Exception {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public Set<String> updateRoutesToCamelContext(CamelContext context) throws Exception {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//}
